c3bbaefc4d1c35434446251bc809540bbbb0cc9c
Gian Moran <gpmoran_@outlook.com>
2019-12-14 20:11:22 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,3 +1,4 @@
+require "pry"
 def join_ingredients(src)
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
@@ -6,28 +7,36 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  new_array = [ ]
+  index = 0
   
- newArray = []
- i= 0
- while i < src.length do 
-   newArray.push("I love #{src[i][0]} and #{src[i][1]} on my pizza")
-   i+=1
- end
-newArray
+  while index < src.count do 
+  
+  new_array << "I love #{src[index][0]} and #{src[index][1]} on my pizza"
+index +=1 
+end
+
+new_array
 
 end
+
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-newArray = []
-i = 0 
-while i < src.length do
-  newArray.push(src[i].max)
-  i+=1
-end
-  newArray
+  new_array = [ ] 
+  row_index = 0 
+  while row_index < src.count do 
+    
+    if(src[row_index][0] > src[row_index][1])
+      new_array << src[row_index][0]
+    else
+      new_array << src[row_index][1]
+  end
+    row_index +=1 
+  end
+new_array
 end
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
@@ -37,15 +46,14 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
- total = 0
- i = 0
- while i < src.length do 
-   if src[i][0] % 2 == 0 && src[i][1] % 2 == 0 
-     total += src[i].sum
-     i+=1
-   else
-     i+=1
-   end
- end
- total
+row_index = 0 
+sum = 0
+while row_index < src.count do 
+  
+  if(src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0)
+    sum += src[row_index][0] + src[row_index][1]
+  end 
+row_index +=1 
+end
+sum
 end
